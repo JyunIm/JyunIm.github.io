d355a0b40b9b45410a6b6e6b50037eb756117c90
JyunIm <114969652+JyunIm@users.noreply.github.com>
2023-05-01 19:38:53 +0900

    Update act.js

--- post/act.js
+++ post/act.js
@@ -1,6 +1,5 @@
-const toggleBtn = document.querySelector('.toggle-btn');
-const hiddenContent = document.querySelector('.hidden-content');
-
-toggleBtn.addEventListener('click', () => {
-  hiddenContent.classList.toggle('show');
+$(function (){
+	$("#btn_toggle").click(function (){
+  	$("#Toggle").toggle();
+  });
 });
